7ce85a7c83c585f1b751f706643e2f7725045b79
Winner Kapela <winnkapela@gmail.com>
2026-02-23 12:45:56 +0100

    last version

--- src/js/script.js
+++ src/js/script.js
@@ -16,8 +16,136 @@
       $('#btn-menu').show()
       $('.bg-bars').hide(50)
       $('body').css({'overflow-y':'auto'})
-    })
-      })
+    });
+
+    
+   
+
+// Bars Button
+var barsBtn = document.getElementById('bars');
+var menu = document.getElementById('NavLink');
+barsBtn.addEventListener('click',()=>{
+    menu.classList.toggle('menuOn');
+});
+      /**Form function */
+
+var $field = $('.field'),
+$username = $('#username'),
+$email = $('#email'),
+$subject = $('#subject'),
+$message = $('#message'),
+$submit = $('#submit-btn'),
+$regName = /^[a-zA-ZéèîïêàâÊ][a-zéèîïêàâç]+([-'\s][a-zA-ZéèîïêàâÊ][a-zéèîïêàâç]+)?/i,
+$regMail = /^[a-z0-9.-_]+@[a-z0-9.-_]+\.[a-z]{2,6}$/i;
+
+$username.keyup(function(e){
+        if($regName.test($username.val())){
+            $($username).addClass('fieldValid');
+                
+            }
+            else if($username.val() =='' || !$regName.test($username.val())){
+              $($username).removeClass('fieldValid');
+            }
+        });
+       
+$email.keyup(function(e){
+        if($regMail.test($email.val())){
+          $($email).addClass('fieldValid');
+                
+            }
+            else if($email.val() =='' || !$regMail.test($email.val())){
+              $($email).removeClass('fieldValid');
+            }
+        });
+$subject.keyup(function(e){
+          if($regName.test($subject.val())){
+            $($subject).addClass('fieldValid');
+                  
+              }
+              else if($subject.val() =='' || !$regName.test($subject.val())){
+                $($subject).removeClass('fieldValid');
+              }
+          });
+$message.keyup(function(e){
+            if($message.val() != ''){
+              $($message).addClass('fieldValid');
+              $message.css({
+                color : 'rgb(0, 172, 0)',
+                border : '2px solid rgb(0, 172, 0)'
+            })
+            }
+            else{
+              $($message).removeClass('fieldValid');
+              $message.css({
+                color : 'red',
+                border : '2px solid red'
+            })
+              e.preventDefault(); 
+              }
+        });
+       
+$submit.click(function(e){
+
+        if($regName.test($username.val())){
+          
+            $($username).addClass('fieldValid');
+              
+            }
+            else if($username.val() =='' || !$regName.test($username.val())){
+        
+                $($username).removeClass('fieldValid');    
+                $username.css({
+                    color : 'red',
+                    border : '2px solid red'
+                })
+                e.preventDefault();
+            }
+        
+        if($regMail.test($email.val())){
+            $($email).addClass('fieldValid');
+                
+            }
+            else if($email.val() =='' || !$regMail.test($email.val())){
+              $($email).removeClass('fieldValid');
+              $email.css({
+                color : 'red',
+                border : '2px solid red'
+            })
+            e.preventDefault();
+            }
+        if($regName.test($subject.val())){
+              $($subject).addClass('fieldValid');
+                    
+                }
+                else if($subject.val() =='' || !$regName.test($subject.val())){
+                  $($subject).removeClass('fieldValid');
+                  $subject.css({
+                    color : 'red',
+                    border : '2px solid red'
+                })
+                e.preventDefault();
+                }
+        if($message.val() == ''){
+            $($message).removeClass('fieldValid');
+            $message.css({
+              color : 'red',
+              border : '2px solid red'
+          })
+            e.preventDefault(); 
+            }
+            else if($message.val() != ''){
+                $($message).addClass('fieldValid');
+                
+            }
+    
+    else{
+        $('form').submit();
+        $('#submit-btn').css({'cursor': 'not-allowed'});
+    }
+     
+});
+
+      });
  
  window.addEventListener("load",()=>{
          const loader = document.getElementById('loader-bg')
@@ -88,35 +216,7 @@ const swipe = new Swiper(".mySwiper", {
       },
 
       1200: {
-        slidesPerView: 3,
-        spaceBetween: 20
-      }
-    }
-  });
-
-  const swiper = new Swiper('.team-slider', {
-    speed: 800,
-    loop: true,
-    autoplay: {
-      delay: 4000,
-      disableOnInteraction: false
-    },
-    slidesPerView: 'auto',
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    },
-    breakpoints: {
-      320: {
-        slidesPerView: 1,
-        spaceBetween: 20
-      },
-      950: {
-                slidesPerView: 2,
-              },
-      1200: {
-        slidesPerView: 3,
+        slidesPerView: 2,
         spaceBetween: 20
       }
     }
@@ -133,3 +233,25 @@ const swipe = new Swiper(".mySwiper", {
       mirror: false
     })
   });
+
+
+//Mode nuit
+
+const btnMode = document.getElementById('BtnMode');
+const PageContent = document.querySelectorAll('body.Page');
+
+//Session Storage
+// Vérifier si le mode nuit est sauvegardé dans sessionStorage
+    if (sessionStorage.getItem('DarkMode') === 'true') {
+        document.body.classList.add('DarkPage');
+    }
+
+btnMode.addEventListener('click', ()=>{ 
+
+    // Basculer la classe 'night-mode'
+      document.body.classList.toggle('DarkPage');
+
+        // Sauvegarder l'état du mode nuit dans sessionStorage
+        const isNightMode = document.body.classList.contains('DarkPage');
+        sessionStorage.setItem('DarkMode', isNightMode);
+});
